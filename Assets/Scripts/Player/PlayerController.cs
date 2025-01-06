d55c783a26ebd5bf21a6650595217cd09f6ab123
FilloPrinci <filloprinci98@gmail.com>
2025-01-06 15:21:13 +0100

    feat: correct accelleration

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float maxSpeed = 300f;
+    public float accelleration = 5f;
+
     private InputManager inputManager;
+    private Vector3 velocity;
+    private float currentSpeed = 0;
 
     private void Start()
     {
@@ -25,19 +30,34 @@ public class PlayerController : MonoBehaviour
         if (inputManager.accellerate())
         {
             // accellerate
-            Debug.Log("Accellerate");
+            velocity = transform.forward * accellerateSpeed(maxSpeed, accelleration) * Time.deltaTime;
         }
         else if (inputManager.brake())
         {
             // brake
-            Debug.Log("Brake");
+            velocity = transform.forward * accellerateSpeed(0, 1) * Time.deltaTime;
         }
         else {
             // brake slowely
+            velocity = transform.forward * accellerateSpeed(0, 1) * Time.deltaTime;
         }
 
+        transform.position += velocity;
 
-        
+    }
+
+    float accellerateSpeed(float targetSpeed, float accelleration) {
+        currentSpeed = expDecay(currentSpeed, targetSpeed, accelleration, Time.deltaTime);
+        if (Mathf.Abs(targetSpeed - currentSpeed) < 0.01)
+        {
+            currentSpeed = targetSpeed;
+        }
+        return currentSpeed;
+    }
+
+    float expDecay(float a, float b, float decay, float deltaTime)
+    {
+        return Mathf.Lerp(a, b, 1 - Mathf.Exp(-decay * deltaTime));
     }
 
     void ApplyGravityAndHover()
