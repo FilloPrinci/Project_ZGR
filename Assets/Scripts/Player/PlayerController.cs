8cbf740e02c92292a0e3c4d69fda0690a612c54b
FilloPrinci <filloprinci98@gmail.com>
2025-01-06 15:57:09 +0100

    feat: steering correctly

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float maxSpeed = 300f;
+    public float rotationMaxSpeed = 10;
+    public float rotationAccelleration = 5;
     public float accelleration = 5f;
 
     private InputManager inputManager;
+
     private Vector3 velocity;
     private float currentSpeed = 0;
+
+    private float rotationVelocity = 0;
+    private float currentRotationSpeed = 0;
+
+    private float deltaTime;
 
     private void Start()
     {
@@ -21,8 +29,30 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        deltaTime = Time.deltaTime;
+
         ApplyGravityAndHover();
+        HandleSteering();
         HandleMovement();
+        
+    }
+
+    void HandleSteering() {
+        float steerInput = inputManager.steer();
+
+        if (steerInput != 0)
+        {
+            // accellerate roation to desired rotation
+            rotationVelocity = AccellerateRotationSpeed(rotationMaxSpeed * steerInput, rotationAccelleration);
+        }
+        else
+        {
+            // brake rotation
+            rotationVelocity = AccellerateRotationSpeed(0, rotationAccelleration);
+        }
+        
+
+        transform.Rotate(0, rotationVelocity * deltaTime, 0, Space.World);
     }
 
     void HandleMovement()
@@ -30,24 +60,34 @@ public class PlayerController : MonoBehaviour
         if (inputManager.accellerate())
         {
             // accellerate
-            velocity = transform.forward * accellerateSpeed(maxSpeed, accelleration) * Time.deltaTime;
+            velocity = transform.forward * AccellerateSpeed(maxSpeed, accelleration) * deltaTime;
         }
         else if (inputManager.brake())
         {
             // brake
-            velocity = transform.forward * accellerateSpeed(0, 1) * Time.deltaTime;
+            velocity = transform.forward * AccellerateSpeed(0, 1) * deltaTime;
         }
         else {
             // brake slowely
-            velocity = transform.forward * accellerateSpeed(0, 1) * Time.deltaTime;
+            velocity = transform.forward * AccellerateSpeed(0, 1) * deltaTime;
         }
 
         transform.position += velocity;
 
     }
 
-    float accellerateSpeed(float targetSpeed, float accelleration) {
-        currentSpeed = expDecay(currentSpeed, targetSpeed, accelleration, Time.deltaTime);
+    float AccellerateRotationSpeed(float targetSpeed, float accelleration)
+    {
+        currentRotationSpeed = ExpDecay(currentRotationSpeed, targetSpeed, accelleration, deltaTime);
+        if (Mathf.Abs(targetSpeed - currentRotationSpeed) < 0.01)
+        {
+            currentRotationSpeed = targetSpeed;
+        }
+        return currentRotationSpeed;
+    }
+
+    float AccellerateSpeed(float targetSpeed, float accelleration) {
+        currentSpeed = ExpDecay(currentSpeed, targetSpeed, accelleration, deltaTime);
         if (Mathf.Abs(targetSpeed - currentSpeed) < 0.01)
         {
             currentSpeed = targetSpeed;
@@ -55,7 +95,7 @@ public class PlayerController : MonoBehaviour
         return currentSpeed;
     }
 
-    float expDecay(float a, float b, float decay, float deltaTime)
+    float ExpDecay(float a, float b, float decay, float deltaTime)
     {
         return Mathf.Lerp(a, b, 1 - Mathf.Exp(-decay * deltaTime));
     }
