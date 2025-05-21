78151eef6933a75146e4d01125e0538e43e51dc7
FilloPrinci <filloprinci98@gmail.com>
2025-05-21 22:28:07 +0200

    feat: developing menus

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -7,26 +7,55 @@ public class MainMenuManager : MonoBehaviour
     [Header("Panels")]
     public GameObject optionsPanel;
     public GameObject mainMenuPanel;
+    public GameObject playOptionsPanel;
+    public GameObject singleplayerOptionsPanel;
+    public GameObject multiplayerOptionsPanel;
 
     [Header("Selectable UI Elements")]
-    public GameObject firstOptionSelected;
     public GameObject firstMainMenuSelected;
+    public GameObject firstOptionSelected;
+    public GameObject firstPlayOptionSelected;
+    public GameObject firstSingleplayerOptionSelected;
+    public GameObject firstMultiplayerOptionSelected;
+    
 
     public string playScene;
 
+    private GameObject currentlyActivePanel;
+
+    private void Start()
+    {
+        currentlyActivePanel = mainMenuPanel;
+    }
+
+    void SwitchPanel(GameObject newPanel, GameObject newSelectedObject)
+    {
+        newPanel.SetActive(true);
+        currentlyActivePanel.SetActive(false);
+        currentlyActivePanel = newPanel;
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(newSelectedObject);
+    }
+
     public void OnPlayPressed()
     {
-        SceneManager.LoadScene(playScene);
+        SwitchPanel(playOptionsPanel, firstPlayOptionSelected);
+    }
+
+    public void OnSingleplayerPressed()
+    {
+        SwitchPanel(singleplayerOptionsPanel, firstSingleplayerOptionSelected);
+    }
+
+    public void OnMultiplayerPressed()
+    {
+        SwitchPanel(multiplayerOptionsPanel, firstMultiplayerOptionSelected);
     }
 
     public void OnOptionsPressed()
     {
-        optionsPanel.SetActive(true);
-        mainMenuPanel.SetActive(false);
-
-        // Imposta la selezione sul primo bottone delle opzioni
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(firstOptionSelected);
+        SwitchPanel(optionsPanel, firstOptionSelected);
     }
 
     public void OnQuitPressed()
@@ -35,13 +64,18 @@ public class MainMenuManager : MonoBehaviour
         Debug.Log("Quit pressed");
     }
 
+    public void OnBackToMainMenu()
+    {
+        SwitchPanel(mainMenuPanel, firstMainMenuSelected);
+    }
+
     public void OnBackFromOptions()
     {
-        optionsPanel.SetActive(false);
-        mainMenuPanel.SetActive(true);
+        SwitchPanel(mainMenuPanel, firstMainMenuSelected);
+    }
 
-        // Imposta la selezione sul primo bottone del menu principale
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(firstMainMenuSelected);
+    public void OnBackFromPlayOptions()
+    {
+        SwitchPanel(mainMenuPanel, firstMainMenuSelected);
     }
 }
